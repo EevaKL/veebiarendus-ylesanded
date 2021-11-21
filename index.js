e0aa65bf5ecaed2fd6cc1c64eff84504b759e2dd
EevaKL <92303905+EevaKL@users.noreply.github.com>
2021-11-21 18:47:45 +0200

    upd123

--- index.js
+++ index.js
@@ -2,9 +2,33 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+function naitaTest(req, res) {
+  res.render('pages/test.ejs')
+}
+
+let ylesanded = [
+  {
+    kirjeldus: "Jaluta koera",
+    tehtud: true
+  },
+  {
+    kirjeldus: "Sööda kassi",
+    tehtud: false
+  },
+  {
+    kirjeldus: "Homikusöök",
+    tehtud: false
+  }
+]
+
+function naitaYlesandeid(req, res) {
+  res.render('pages/ylesanded.ejs', { ylesanded: ylesanded })
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', naitaYlesandeid)
+  .get('/proov', naitaTest)
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
